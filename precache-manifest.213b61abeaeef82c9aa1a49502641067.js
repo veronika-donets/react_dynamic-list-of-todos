dd693d2038a6ef959ac7f176d5347a6f04335024
Veronika D <donets.veronika@yahoo.com>
2019-11-05 22:07:24 +0200

    Updates

---
+++ precache-manifest.213b61abeaeef82c9aa1a49502641067.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6831acea71942377c578b6570874d6a7",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "bdae084e4f88be73893a",
+    "url": "/react_dynamic-list-of-todos/static/css/main.1c5e87b6.chunk.css"
+  },
+  {
+    "revision": "f663305420d5f3671afd",
+    "url": "/react_dynamic-list-of-todos/static/js/2.863d6061.chunk.js"
+  },
+  {
+    "revision": "bdae084e4f88be73893a",
+    "url": "/react_dynamic-list-of-todos/static/js/main.9de878d9.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
